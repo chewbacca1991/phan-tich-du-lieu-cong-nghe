cccc3f508350c01c1808f0ccb98db16b7a01a466
chewbacca1991 <mounafbanbo@hotmail.com>
2025-09-07 23:01:07 +0330

    Fix label issue in x-axis of DataVisualizer plot method.

--- visualize_data.py
+++ visualize_data.py
@@ -8,7 +8,7 @@ class DataVisualizer:
     def plot(self):
         x = np.arange(len(self.data))
         plt.bar(x, self.data)
-        plt.xticks(x, [f'Item {i}' for i in range(len(self.data))])
+        plt.xticks(x, [f'Item {i + 1}' for i in range(len(self.data))])  # Changed to start from 1
         plt.xlabel('Data')
         plt.ylabel('Value')
         plt.title('Technology Data Chart')
